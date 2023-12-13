1f491d024a6b90eb504babde9be9d4cb0b1c499c
GoHyunkyu <gusrb0296@naver.com>
2023-12-13 20:22:13 +0900

    Squashed commit of the following:
    commit 269e1e84379ba8cca36eb71f8009871fd85dc50d
    Author: GoHyunkyu <gusrb0296@naver.com>
    Date:   Wed Dec 13 19:26:55 2023 +0900
    
        Squashed commit of the following:
    
        commit 9a9558bca786520e658e95aa21f11629482d1e53
        Author: GoHyunkyu <gusrb0296@naver.com>
        Date:   Wed Dec 13 19:18:18 2023 +0900
    
            Feat: main funtion
    
            필수 구현 기능에 필요한 모든 기능들을 완성했습니다.
            단, 모든 기능을 GameManager에 넣었으며 객체화, 분리작업, 중복기능 제거를 하기 전입니다.
    
    commit 6ef583e157e0090acd7c6ba3d1e9c714b5084e98
    Author: GoHyunkyu <gusrb0296@naver.com>
    Date:   Tue Dec 12 19:53:05 2023 +0900
    
        Feat: Funtion Deposit
    
        버튼 클릭 시 UI팝업 기능
        입금 돈 버튼 클릭 시 해당 가격이 Balance에 들어갑니다.
        에러) 각 버튼이 가지고 있는 Balance 값이 다릅니다.
    
    commit b29dc394ada84f65525f4db490ee6383f7b1d133
    Author: GoHyunkyu <gusrb0296@naver.com>
    Date:   Tue Dec 12 15:42:17 2023 +0900
    
        Squashed commit of the following:
    
        commit 6b7efea58aa2d468a40fd7d451c7208c7dc20993
        Author: GoHyunkyu <gusrb0296@naver.com>
        Date:   Tue Dec 12 15:41:47 2023 +0900
    
            Feat: Make Main UI
    
            메인 화면에 필요한 UI들을 만들었습니다.
    
    commit af6b146b84e837036a67e02415e88b0b7e07a9f1
    Author: GoHyunkyu <gusrb0296@naver.com>
    Date:   Tue Dec 12 10:23:39 2023 +0900
    
        Feat: create folder
    
        Scipts와 Prefabs 폴더를 생성했습니다

---
+++ ATM/Assets/Scrpts/GameManager.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using TMPro.EditorUtilities;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public TextMeshProUGUI HoldingMoneyText;
+    public TextMeshProUGUI BalanceMoneyText;
+
+    public GameObject LackOfMouneyPopUp;
+
+    public TMP_InputField MoneyInputField;
+
+
+    int HoldingMoney = 100000;
+    int BalanceMoney = 50000;
+
+
+    private void Awake()
+    {
+        instance = this;
+        AllMoneyUpdate();
+    }
+
+    public void CheckEnoughWithdraw(int money)
+    {
+        if(BalanceMoney < money)
+        {
+            LackOfMouneyPopUp.SetActive(true);
+        }
+        else
+        {
+            Withdraw(money);
+        }
+    }
+    public void CheckEnoughDeposit(int money)
+    {
+        if(HoldingMoney < money)
+        {
+            LackOfMouneyPopUp.SetActive(true);
+        }
+        else
+        {
+            Deposit(money);
+        }
+    }
+
+    public void Deposit(int money)
+    {
+        BalanceMoney += money;
+        HoldingMoney -= money;
+        AllMoneyUpdate();
+    }
+
+    public void Withdraw(int money)
+    {
+        BalanceMoney -= money;
+        HoldingMoney += money;
+        AllMoneyUpdate();
+    }
+
+    public void AllMoneyUpdate()
+    {
+        BalanceMoneyText.text = BalanceMoney.ToString("C");
+        HoldingMoneyText.text = HoldingMoney.ToString("C"); 
+    }
+
+    public void DepositInputFieldMoney()
+    {
+        string money = MoneyInputField.text;
+        if(int.TryParse(money, out int num))
+        {
+            CheckEnoughDeposit(Convert.ToInt32(money));
+        }
+    }
+
+    public void WithdarwInputFieldMoney()
+    {
+        string money = MoneyInputField.text;
+        if (int.TryParse(money, out int num))
+        {
+            CheckEnoughWithdraw(Convert.ToInt32(money));
+        }
+    }
+}
